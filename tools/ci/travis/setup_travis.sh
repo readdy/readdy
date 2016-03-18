073563299f70c958104a883155eef0471fbb915d
Moritz Hoffmann <clonker@gmail.com>
2016-03-18 16:05:26 +0100

    [travis] update cmake to 3.3.2

--- tools/ci/travis/setup_travis.sh
+++ tools/ci/travis/setup_travis.sh
@@ -2,7 +2,7 @@
 
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
   CMAKE_URL="http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz"
-  mkdir _cmake && wget --quiet -O - ${CMAKE_URL} | tar --strip-components=1 -xz -C _cmake
+  mkdir _cmake && wget --quiet ${CMAKE_URL} -O - | tar --strip-components=1 -xz -C _cmake
   export PATH=${DEPS_DIR}/_cmake/bin:${PATH}
 else
   brew install cmake
