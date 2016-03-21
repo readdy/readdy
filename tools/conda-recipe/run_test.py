c78a85f0ca59666011b923ca2477d694793f4c8e
Moritz Hoffmann <clonker@gmail.com>
2016-03-21 11:04:31 +0100

    wip

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -5,7 +5,7 @@ import sys
 
 src_dir = os.getenv('SRC_DIR')
 
-test_command = "cd %s/build; make runUnitTests; make test ARGS=\"-V\"; cd -" % src_dir
+test_command = "%s/out/bin/runUnitTests" % src_dir
 print("Test command = %s" % test_command)
 command_result = subprocess.call(test_command, shell=True)
 print("ran test command")
