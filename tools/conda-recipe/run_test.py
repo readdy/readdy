8b0c83e2059865ee0df67ac88146c0b3f1afee7e
Moritz Hoffmann <clonker@gmail.com>
2016-03-21 11:46:16 +0100

    wip

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -4,7 +4,9 @@ import subprocess
 import sys
 
 src_dir = os.getenv('SRC_DIR')
-
+print("***"*30)
+subprocess.call("env | sort")
+print("***"*30)
 test_command = "runUnitTests"
 print("Test command = %s" % test_command)
 command_result = subprocess.call(test_command, shell=True)
