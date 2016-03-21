ff43289b533c83f57bfc19e12e5267dbc544d0e9
Moritz Hoffmann <clonker@gmail.com>
2016-03-21 11:15:51 +0100

    wip

--- tools/conda-recipe/run_test.py
+++ tools/conda-recipe/run_test.py
@@ -5,7 +5,7 @@ import sys
 
 src_dir = os.getenv('SRC_DIR')
 
-test_command = "%s/out/bin/runUnitTests" % src_dir
+test_command = "runUnitTests"
 print("Test command = %s" % test_command)
 command_result = subprocess.call(test_command, shell=True)
 print("ran test command")
