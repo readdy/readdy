ddd7de99fc38f8aa0e4e18a9188b2423402d7182
Moritz Hoffmann <clonker@gmail.com>
2016-03-21 14:04:53 +0100

    wip

--- tools/ci/travis/setup_travis.sh
+++ tools/ci/travis/setup_travis.sh
@@ -3,13 +3,15 @@ set -evx
 env | sort
 cd $HOME
 if [[ "${TRAVIS_OS_NAME}" == "linux" ]]; then
-  CMAKE_URL="http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz"
-  mkdir -p _cmake
-  wget ${CMAKE_URL} --no-check-certificate -q -O _cmake/__cmake.tar.gz
-  for file in _cmake/*.tar.gz; do tar -xzf "${file}" --strip=1 -C _cmake && rm "${file}"; done
+  #CMAKE_URL="http://www.cmake.org/files/v3.3/cmake-3.3.2-Linux-x86_64.tar.gz"
+  #mkdir -p _cmake
+  #wget ${CMAKE_URL} --no-check-certificate -q -O _cmake/__cmake.tar.gz
+  #for file in _cmake/*.tar.gz; do tar -xzf "${file}" --strip=1 -C _cmake && rm "${file}"; done
+  echo "skip this since cmake gets provided by conda"
 else
   brew update >/dev/null
-  brew unlink cmake
+  # brew unlink cmake
+  # brew install cmake
   brew unlink gcc
-  brew install cmake gcc
+  brew install gcc
 fi
