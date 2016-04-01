cdf89128e5fca5cbf10de390ff7b3beb861f2dfb
Moritz Hoffmann <clonker@gmail.com>
2016-04-01 15:26:08 +0200

    wip

--- tools/conda-recipe/build.sh
+++ tools/conda-recipe/build.sh
@@ -4,22 +4,7 @@ CMAKE_FLAGS="-DCMAKE_INSTALL_PREFIX=$PREFIX -DBUILD_TESTING=OFF"
 CMAKE_FLAGS+=" -DREADDY_BUILD_SHARED_COMBINED:BOOL=ON"
 CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
-if [ `uname` == Darwin ]; then
-    if [ $PY3K -eq 1 ]; then
-        cd $LIBRARY_PATH
-        ln -s libpython${PY_VER}m.dylib libpython${PY_VER}.dylib
-        cd -
-    fi
-    CMAKE_FLAGS+=" -DPYTHON_LIBRARY:PATH=$PREFIX/lib/libpython${PY_VER}.dylib"
-fi
-if [ `uname` == Linux ]; then
-    if [ $PY3K -eq 1 ]; then
-        cd $LIBRARY_PATH
-        ln -s libpython${PY_VER}m.so libpython${PY_VER}.so
-        cd -
-    fi
-    CMAKE_FLAGS+=" -DPYTHON_LIBRARY:PATH=$PREFIX/lib/libpython${PY_VER}.so"
-fi
+export PYTHON_INCLUDE_DIR=`python -c "from __future__ import print_function; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc(True))"`
 
 # cant reliably determine cpu count in a docker container,
 # therefore fix this value.
