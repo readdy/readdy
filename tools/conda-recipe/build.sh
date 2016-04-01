5bf0ba0fddc97708a102e1687d55d5d178cb581e
Moritz Hoffmann <clonker@gmail.com>
2016-04-01 15:59:24 +0200

    wip

--- tools/conda-recipe/build.sh
+++ tools/conda-recipe/build.sh
@@ -6,6 +6,24 @@ CMAKE_FLAGS+=" -DCMAKE_BUILD_TYPE=Release"
 
 export PYTHON_INCLUDE_DIR=`python -c "from __future__ import print_function; import distutils.sysconfig; print(distutils.sysconfig.get_python_inc(True))"`
 
+# attempt to feed the right python library to FindPythonLibs
+if [ `uname` == Darwin ]; then
+    if [ $PY3K -eq 1 ]; then
+        cd $LIBRARY_PATH
+        ln -s libpython${PY_VER}m.dylib libpython${PY_VER}.dylib
+        cd -
+    fi
+    CMAKE_FLAGS+=" -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.dylib"
+fi
+if [ `uname` == Linux ]; then
+    if [ $PY3K -eq 1 ]; then
+        cd $LIBRARY_PATH
+        ln -s libpython${PY_VER}m.so libpython${PY_VER}.so
+        cd -
+    fi
+    CMAKE_FLAGS+=" -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/libpython${PY_VER}.so"
+fi
+
 # cant reliably determine cpu count in a docker container,
 # therefore fix this value.
 if [ "$TRAVIS" == "true" ]; then CPU_COUNT=2; fi
