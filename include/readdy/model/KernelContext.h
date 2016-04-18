c88268d3074ff9615fadd24dd46b94050c04f3f3
Moritz Hoffmann <clonker@gmail.com>
2016-04-18 14:35:32 +0200

    added model subpackage + kernelcontext for time independent data

---
+++ include/readdy/model/KernelContext.h
@@ -0,0 +1,26 @@
+/**
+ * << detailed description >>
+ *
+ * @file KernelContext.h
+ * @brief Container class for time independent information of the simulation.
+ * @author clonker
+ * @date 18.04.16
+ */
+
+#ifndef READDY2_MAIN_KERNELCONTEXT_H
+#define READDY2_MAIN_KERNELCONTEXT_H
+
+namespace readdy {
+    namespace model {
+        class KernelContext {
+        protected:
+            double kBT;
+
+        public:
+            double getKBT() const;
+
+            void setKBT(double kBT);
+        };
+    }
+}
+#endif //READDY2_MAIN_KERNELCONTEXT_H
---
+++ readdy2/main/model/KernelContext.cpp
@@ -0,0 +1,24 @@
+/**
+ * << detailed description >>
+ *
+ * @file KernelContext.cpp
+ * @brief Implementation file of KernelContext.h
+ * @author clonker
+ * @date 18.04.16
+ * @todo make proper reference to KernelContext.h, is kBT really indepdendent of t?
+ */
+
+#include <readdy/model/KernelContext.h>
+
+using namespace readdy::model;
+
+double KernelContext::getKBT() const  {
+    return KernelContext::kBT;
+}
+
+void KernelContext::setKBT(double kBT) {
+    KernelContext::kBT = kBT;
+}
+
+
+
