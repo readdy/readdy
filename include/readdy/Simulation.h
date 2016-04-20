59bde5df35ff2eb971c2c7a06dc535af7b2e153f
Moritz Hoffmann <clonker@gmail.com>
2016-04-20 11:26:39 +0200

    wip

--- include/readdy/Simulation.h
+++ include/readdy/Simulation.h
@@ -25,19 +25,29 @@ namespace readdy {
     class Simulation {
     public:
         Simulation();
+
         ~Simulation();
+
         // move
         Simulation(Simulation &&rhs);
-        Simulation& operator=(Simulation &&rhs);
+
+        Simulation &operator=(Simulation &&rhs);
+
         // copy
         Simulation(const Simulation &rhs);
-        Simulation& operator=(const Simulation &rhs);
+
+        Simulation &operator=(const Simulation &rhs);
 
         double getKBT() const;
+
         void setKBT(double kBT);
+
         std::array<double, 3> getBoxSize() const;
+
         void setBoxSize(double dx, double dy, double dz);
+
         std::array<bool, 3> getPeriodicBoundary() const;
+
         void setPeriodicBoundary(bool pb_x, bool pb_y, bool pb_z);
 
         void registerParticleType(const std::string name, const double diffusionCoefficient);
@@ -46,10 +56,15 @@ namespace readdy {
         //void registerReactionByDescriptor(const std::string descriptor);
 
         void addParticle(double x, double y, double z, std::string type);
+
         std::vector<std::array<double, 3>> getParticlePositions();
+
         void setKernel(const std::string kernel);
+
         bool isKernelSet() const;
+
         std::string getSelectedKernelType() const;
+
         virtual void run(const size_t steps, const double timeStep);
 
     private:
