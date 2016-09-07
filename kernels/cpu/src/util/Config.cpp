fcf930730b67a528b777810c57bbb27a379fd752
Moritz Hoffmann <clonker@gmail.com>
2016-09-07 17:08:16 +0200

    made n threads of cpu kernel customizable by environment variable

--- kernels/cpu/src/util/Config.cpp
+++ kernels/cpu/src/util/Config.cpp
@@ -8,6 +8,7 @@
  */
 
 #include <thread>
+#include <boost/log/trivial.hpp>
 #include "readdy/kernel/cpu/util/Config.h"
 
 namespace readdy {
@@ -16,6 +17,12 @@ namespace readdy {
             namespace util {
                 Config::Config() {
                     nThreads = std::thread::hardware_concurrency();
+                    const char *env = std::getenv("READDY_N_CORES");
+                    if(env) {
+                        nThreads = static_cast<unsigned long>(std::stol(env));
+                        BOOST_LOG_TRIVIAL(debug) << "Using "
+                                                 << nThreads << " threads (by environment variable READDY_N_CORES)";
+                    }
                 }
             }
         }
