20d96c40234df63f30a98cddca43890b38b85393
Moritz Hoffmann <clonker@gmail.com>
2016-09-14 14:00:50 +0200

    [docs] switched output repo for doxygen

--- tools/ci/travis/make_docs.sh
+++ tools/ci/travis/make_docs.sh
@@ -4,21 +4,21 @@
 set -e -u
 
 function set_this_up {
-    if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-    then
-        echo "This was a pull request, thus dont build docs. Exit."
-        exit 0
-    fi
-    if [ "$TRAVIS_BRANCH" != "master" ]
-    then
-        echo "This commit was made against the $TRAVIS_BRANCH branch and not the master branch. Exit."
-        exit 0
-    fi
-    if [ "$CONDA_PY" != "27" ]
-    then
-        echo "Only build documentation for python version 2.7. Exit."
-        exit 0
-    fi
+    #if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+    #then
+    #    echo "This was a pull request, thus dont build docs. Exit."
+    #    exit 0
+    #fi
+    #if [ "$TRAVIS_BRANCH" != "master" ]
+    #then
+    #    echo "This commit was made against the $TRAVIS_BRANCH branch and not the master branch. Exit."
+    #    exit 0
+    #fi
+    #if [ "$CONDA_PY" != "27" ]
+    #then
+    #    echo "Only build documentation for python version 2.7. Exit."
+    #    exit 0
+    #fi
     if [ "${TRAVIS_OS_NAME}" != "linux" ]
     then
         echo "Only build documentation for linux. Exit."
@@ -49,7 +49,7 @@ function setup_docs_repo {
     git init
     git config user.name "Moritz Hoffmann"
     git config user.email "clonker@users.noreply.github.com"
-    git remote add upstream "https://$GH_TOKEN@github.com/readdy/readdy.git"
+    git remote add upstream "https://$GH_TOKEN@github.com/readdy/readdy_documentation.git"
     git fetch upstream
     git checkout --orphan workbranch
     git reset --hard
@@ -58,8 +58,6 @@ function setup_docs_repo {
 
 function deploy {
     # revision tag
-    cd ${TRAVIS_BUILD_DIR}
-    rev=$(git rev-parse --short HEAD)
     cd $HOME/_readdy_docs/docs/html
     touch .
     git add -A .
