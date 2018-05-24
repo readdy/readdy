60de7e7e06e5a3f349200d847952f6e21d603350
chrisfroe <christoph.froehner@fu-berlin.de>
2018-05-24 13:50:19 +0200

    [integration] init

---
+++ readdy/test/TestIntegration.cpp
@@ -0,0 +1,33 @@
+/********************************************************************
+ * Copyright © 2018 Computational Molecular Biology Group,          *
+ *                  Freie Universität Berlin (GER)                  *
+ *                                                                  *
+ * This file is part of ReaDDy.                                     *
+ *                                                                  *
+ * ReaDDy is free software: you can redistribute it and/or modify   *
+ * it under the terms of the GNU Lesser General Public License as   *
+ * published by the Free Software Foundation, either version 3 of   *
+ * the License, or (at your option) any later version.              *
+ *                                                                  *
+ * This program is distributed in the hope that it will be useful,  *
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of   *
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the    *
+ * GNU Lesser General Public License for more details.              *
+ *                                                                  *
+ * You should have received a copy of the GNU Lesser General        *
+ * Public License along with this program. If not, see              *
+ * <http://www.gnu.org/licenses/>.                                  *
+ ********************************************************************/
+
+
+/**
+ * « detailed description »
+ *
+ * @file TestIntegration.cpp
+ * @brief « brief description »
+ * @author chrisfroe
+ * @date 24.05.18
+ */
+
+#include <readdy/testing/Utils.h>
+#include <readdy/common/integration.h>
