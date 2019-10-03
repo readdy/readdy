2a3eba5a3e478fdfefd44b0bd0674fb1bb4dc51f
Moritz Hoffmann <clonker@gmail.com>
2019-10-04 01:25:54 +0200

    f

--- tools/ci/travis/install_miniconda.sh
+++ tools/ci/travis/install_miniconda.sh
@@ -11,7 +11,7 @@ function install_miniconda {
         elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
              platform="MacOSX"
         fi
-	wget http://repo.continuum.io/miniconda/Miniconda3-latest-$platform-x86_64.sh -O mc.sh -o /dev/null
+	wget http://repo.continuum.io/miniconda/Miniconda3-4.6.14-$platform-x86_64.sh -O mc.sh -o /dev/null
 	bash mc.sh -b -f -p $TARGET
 }
 
