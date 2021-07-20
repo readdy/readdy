bc088fa90fb8154a080e4d98ae17ffbfac95a2f6
Moritz Hoffmann <clonker@gmail.com>
2021-07-20 10:43:20 +0200

    CI finalize (#204)

--- tools/ci/github/upload_conda_package.sh
+++ tools/ci/github/upload_conda_package.sh
@@ -51,23 +51,32 @@ function validate_this_should_run() {
 }
 
 # fixme removethis: this is up here for debugging the build pipeline
-resolve_github_ref
-echo "tag is ${tag}"
-echo "branch is ${branch}"
+# resolve_github_ref
+# echo "tag is ${tag}"
+# echo "branch is ${branch}"
 
-validate_this_should_run
+# validate_this_should_run
 
 conda_package_file=$(conda build tools/conda-recipe --python="${PYTHON_VERSION}" --output | grep '.tar.bz2' | tail -1)
 echo "Found conda package file ${conda_package_file}"
 
 conda install anaconda-client -qy
 
-if [ "${tag}" == "notag" ]; then
-  echo "Uploading package ${conda_package_file} to dev channel"
-  anaconda -t "${BINSTAR_TOKEN}" upload -c readdy -u readdy -l dev --force "${conda_package_file}" > /dev/null 2>&1
+if [ "${BUILD_REASON}" == "IndividualCI" ]; then
+  output=$(git describe --tags --exact-match "${BUILD_SOURCE_VERSION}")
+  echo "Git describe output: ${output}"
+  status=$?
+  if [ ${status} == 128 ]; then
+    echo "Git describe status 128, not a tag."
+    echo "Uploading package ${conda_package_file} to dev channel"
+    anaconda -t "${BINSTAR_TOKEN}" upload -c readdy -u readdy -l dev --force "${conda_package_file}" > /dev/null 2>&1
+  else
+    echo "Git describe successful with tag ${output}."
+    echo "Uploading tagged package ${conda_package_file} with tag ${tag} to regular channel"
+    anaconda -t "${BINSTAR_TOKEN}" upload -u readdy "${conda_package_file}" > /dev/null 2>&1
+  fi
 else
-  echo "Uploading tagged package ${conda_package_file} with tag ${tag} to regular channel"
-  anaconda -t "${BINSTAR_TOKEN}" upload -u readdy "${conda_package_file}" > /dev/null 2>&1
+  echo "Skipping upload because the build reason was ${BUILD_REASON} and not 'IndividualCI'"
 fi
 
 echo "Uploaded. Done."
