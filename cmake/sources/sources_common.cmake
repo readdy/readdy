13cd65a73096b29157cfc703494d21a8087832a6
Moritz Hoffmann <clonker@gmail.com>
2021-10-04 16:09:34 +0200

    remove some submodules

--- cmake/sources/sources_common.cmake
+++ cmake/sources/sources_common.cmake
@@ -1,15 +1,12 @@
 set(SOURCES_DIR "${READDY_GLOBAL_DIR}/readdy/main/common")
 
-set(SPDLOG_INCLUDE_DIR "${READDY_GLOBAL_DIR}/libraries/spdlog/include")
-set(JSON_INCLUDE_DIR "${READDY_GLOBAL_DIR}/libraries/json/include")
-
 find_package(Threads REQUIRED)
 
 # include dirs
-set(COMMON_INCLUDE_DIRS "${READDY_GLOBAL_INCLUDE_DIR};${SPDLOG_INCLUDE_DIR};${JSON_INCLUDE_DIR}" CACHE INTERNAL "Common include dirs")
+set(COMMON_INCLUDE_DIRS "${READDY_GLOBAL_INCLUDE_DIR}" CACHE INTERNAL "Common include dirs")
 
 # dependent libraries
-set(READDY_COMMON_LIBRARIES "graphs;fmt::fmt-header-only" CACHE INTERNAL "Common libraries")
+set(READDY_COMMON_LIBRARIES "graphs;fmt::fmt-header-only;nlohmann_json::nlohmann_json;spdlog;blosc" CACHE INTERNAL "Common libraries")
 
 # sources
 list(APPEND READDY_COMMON_SOURCES "${SOURCES_DIR}/../api/KernelConfiguration.cpp")
