14d27706161ce786d7ea490372249eca1d6b7ade
Moritz Hoffmann <clonker@gmail.com>
2022-03-30 14:48:51 +0200

    fix

--- wrappers/python/src/python/readdy/util/platform_utils.py
+++ wrappers/python/src/python/readdy/util/platform_utils.py
@@ -1,7 +1,7 @@
 def get_readdy_plugin_dir():
     import os
     from pathlib import Path
-    if os.environ["READDY_PLUGIN_DIR"]:
+    if "READDY_PLUGIN_DIR" in os.environ.keys():
         return Path(os.environ["READDY_PLUGIN_DIR"]).resolve()
     else:
         return (Path(os.environ["CONDA_PREFIX"]) / 'lib' / 'readdy_plugins').resolve()
