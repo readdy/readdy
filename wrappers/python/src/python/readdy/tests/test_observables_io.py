a7d84026ff022a8c20012ad3c7c7cb25dba7dc71
Moritz Hoffmann <clonker@gmail.com>
2022-04-21 11:41:48 +0200

    f

--- wrappers/python/src/python/readdy/tests/test_observables_io.py
+++ wrappers/python/src/python/readdy/tests/test_observables_io.py
@@ -63,8 +63,6 @@ from readdy.util.testing_utils import ReaDDyTestCase
 class TestObservablesIO(ReaDDyTestCase):
     @classmethod
     def setUpClass(cls):
-        cls.kernel_provider = KernelProvider.get()
-        cls.kernel_provider.load_from_dir(str(platform_utils.get_readdy_plugin_dir()))
         cls.dir = tempfile.mkdtemp("test-observables-io")
 
     @classmethod
